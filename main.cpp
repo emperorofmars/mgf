98c3012dd96d9e82c9af713c1d42eac8e9ca4c7d
Martin Schwarz <martin.schwarz10@gmail.com>
2015-04-25 16:06:21 +0200

    better main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 int main(int argc, char *argv[]){
 	LOG_INIT("log_mgf.txt", true);
 
+//###############################################  Create necessary objects
 	std::shared_ptr<mgf::Window> w(new mgf::Window("MGF Test", 1000, 800, 0, 0));
 	std::shared_ptr<mgf::Input> input(new mgf::Input);
 
@@ -18,33 +19,23 @@ int main(int argc, char *argv[]){
 	p->createProgram();
 
 	std::shared_ptr<mgf::Camera> cam(new mgf::Camera(90, w->getAspectRatio(), 0.1f, 1000.f, CAMERA_FPS_EULER, 0.6f, 0.4f));
-
 	std::shared_ptr<mgf::Renderer> renderer(new mgf::Renderer(w, cam, p));
 
+//###############################################  Load 3d files
 	mgf::Loader l;
-	std::shared_ptr<mgf::Node> root = l.load("res/models/cube/cube.obj"); //"res/models/scene/scene.obj"
-	if(!root){
-		LOG_F_ERROR(MGF_LOG_FILE, "Loading failed, exiting main!");
-		return 0;
-	}
+	std::shared_ptr<mgf::Node> root(new mgf::Node());
+	root->add(l.load("res/models/cube/cube.obj"));
 	root->add(l.load("res/models/scene/scene.obj"));
-/*
-	std::shared_ptr<mgf::Node> node(new mgf::Node("root"));
-	node->add(std::shared_ptr<mgf::Node>(new mgf::MeshNode("child1")));
-	node->print();
-	node->translate(glm::vec3(0.f, 1.f, 0.f));
-	std::shared_ptr<mgf::MeshNode> tmp = std::dynamic_pointer_cast<mgf::MeshNode>(node->getChild("child1"));
-	tmp->translate(glm::vec3(5.f, 0.f, 0.f));
-	tmp->rotate(20.f, glm::vec3(0.3f, 1.f, 0.1f));
-	tmp->scale(glm::vec3(4.f, 1.f, 1.f));
-	tmp->addMesh(mgf::createCube());
+	root->print();
 
-	std::cout << "TRANSLATION: " << mgf::vec3_toStr(tmp->getGlobalTranslation()) << std::endl;
-	std::cout << "ROTATION: " << std::endl << mgf::mat4_toStr(tmp->getGlobalRotation()) << std::endl;
-	std::cout << "TRS: " << std::endl << mgf::mat4_toStr(tmp->getGlobalTRS()) << std::endl;
-*/
+//###############################################  clone objects into szene
+	std::shared_ptr<mgf::Node> actualScene(new mgf::Node());
+	actualScene->add(root->getChild("scene.obj")->getChild("Cube")->clone());
+	actualScene->add(root->getChild("scene.obj")->getChild("Suzanne")->clone());
+	actualScene->print();
 
-	SDL_SetRelativeMouseMode(SDL_TRUE);
+//###############################################  transform objects
+	actualScene->getChild("Suzanne")->scale(glm::vec3(2.f, 3.f, 4.f));
 
 	w->use();
 	p->use();
@@ -57,16 +48,16 @@ int main(int argc, char *argv[]){
 		input->update();
 		quit = input->getQuit();
 		cam->update(input->getPos(), input->getRot());
+
+		actualScene->getChild("Cube")->rotate(0.2f, glm::vec3(0.f,1.f, 0.f)); //rotate the cube
 //###############################################  Rendering
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glClearBufferfv(GL_COLOR, 0, glm::value_ptr(glm::vec4(0.3f, 0.3f, 0.3f, 1.0f)));
 
-		SDL_Delay(16);
+		actualScene->render(renderer); //rendering on gpu happens here
 
-		root->render(renderer);
-
-		w->swap();
-
+		w->swap(); //display the rendered image on screen
+//###############################################  Calculate fps
 		current = SDL_GetTicks();
 		frametime = current - last;
 		last = current;
